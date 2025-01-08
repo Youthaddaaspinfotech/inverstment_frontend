18c8a0f9f189c368fd2f07e53912505518435c71
Sanjeev Sharma <sanjeev.sharma@keensolution.in>
2025-01-08 18:10:07 +0530

    changes helper file code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
@@ -12,9 +12,9 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+  <HashRouter >
+    <App />
+  </HashRouter >
 );
 
 // If you want to start measuring performance in your app, pass a function
